7d3fffb98c87c65541ad227d65da19722c222b27
danielsun <danielsun12345@gmail.com>
2020-08-05 12:55:38 -0700

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,27 @@
+  
+class PokemonsController < ApplicationController
+    def index
+      pokemons = Pokemon.all
+      render json: PokemonSerializer.new(pokemons)
+    end
+  
+    def create
+      new_poke = Pokemon.new(JSON(request.body.read))
+      new_poke.nickname = Faker::Name.first_name
+      new_poke.species = Faker::Games::Pokemon.name
+      new_poke.save
+  
+      render json: PokemonSerializer.new(new_poke)
+    end
+  
+    def show
+      pokemon = Pokemon.find_by(id: params[:id])
+      render json: PokemonSerializer.new(pokemon)
+    end
+  
+    def destroy
+      # binding.pry
+      Pokemon.find_by(id: params[:id]).destroy
+      render json: { message: "pokemon #{params[:id]} deleted" }
+    end
+  end
