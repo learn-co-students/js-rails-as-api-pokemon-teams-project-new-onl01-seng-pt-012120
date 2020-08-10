7091273786923e98342e5ac103ff9c63e41356de
hornadam8 <hornadam8@gmail.com>
2020-08-10 03:15:50 -0600

    Done.

---
+++ pokemon-teams-backend/app/serializers/trainer_serializer.rb
@@ -0,0 +1,13 @@
+class TrainerSerializer
+  attr_reader :data
+
+  def initialize(trainer_data)
+    @data = trainer_data
+  end
+
+  def to_serialized_json
+    self.data.to_json(include: {
+      pokemons: {except: [:created_at, :updated_at]}
+      }, except:[:created_at, :updated_at])
+  end
+end
